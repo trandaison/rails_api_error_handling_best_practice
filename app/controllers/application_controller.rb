aa27416a740e673dd8c35949c221b21e53574e49
Son Tran <sontd.it@gmail.com>
2018-09-05 21:56:41 +0700

    Step 2: Rescue the exception from scratch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  protected
+  def render_unprocessable_entity_response error, status: :unprocessable_entity
+    render json: error.record.errors, status: status
+  end
 end
