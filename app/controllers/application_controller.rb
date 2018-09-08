3e109a56997e205ad7f495b20c7c7f3c22323e43
Son Tran <sontd.it@gmail.com>
2018-09-08 11:24:36 +0700

    Step 5: Add runtime error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,21 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from Errors::Runtime::StandarError,
+    Errors::Runtime::ActionFailed,
+    Errors::Runtime::ServiceFailed,
+    with: :render_runtime_error_response
 
   protected
   def render_unprocessable_entity_response error, status: :unprocessable_entity
-    render json: Errors::ActiveRecordValidation.new(error.record), status: status
+    render json: Errors::ActiveRecordValidation.new(error.record).to_hash, status: status
   end
 
   def render_404 error, status: :not_found
-    render json: Errors::ActiveRecordNotFound.new(error), status: status
+    render json: Errors::ActiveRecordNotFound.new(error).to_hash, status: status
+  end
+
+  def render_runtime_error_response error, status: :bad_request
+    render json: error.to_hash, status: status
   end
 end
